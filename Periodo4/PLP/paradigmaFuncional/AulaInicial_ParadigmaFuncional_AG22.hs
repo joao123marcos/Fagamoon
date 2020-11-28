19bc0c13d454b66689363a249d331e8b848caaca
joao <you@example.com>
2020-11-28 11:37:55 -0300

    atualizações

---
+++ Periodo4/PLP/paradigmaFuncional/AulaInicial_ParadigmaFuncional_AG22.hs
@@ -0,0 +1,23 @@
+--função soma
+soma x y = x + y
+
+-- função que incrementa o valor de entrada
+incremento z = z + 1
+
+--area do triangulo
+areaTriangulo b h = (b*h)/ 2
+
+--area circulo
+areaCirculo r = pi * r * r
+
+--Calcular a media
+media x y z = (x + y + z)/3
+
+--Calcular o dobro
+dobro x = x*2
+
+--Calcular o quadruplo
+quadruplo x = (dobro x) * 2
+
+--Calcular hipotenusa
+hipotenusa x y = sqrt (x**2 + y**2)
