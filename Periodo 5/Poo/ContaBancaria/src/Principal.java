6573236ec4742ad82d31ee364f988a9ba002a045
joao123marcos <joaomarcos.spires@gmail.com>
2021-03-02 22:21:23 -0300

    classe extends

--- Periodo 5/Poo/ContaBancaria/src/Principal.java
+++ Periodo 5/Poo/ContaBancaria/src/Principal.java
@@ -29,6 +29,29 @@ public class Principal {
         System.out.println("O salario desse funcionairo é "+ joao.getSalarioFuncionario());
         System.out.println(" ");
         System.out.println("O endereço desse funcionario é "+ joao.getEnderecoFuncionario());
+        
+        System.out.println(" ");
+        System.out.println("Criando um gerente............");
+        
+        System.out.println("Informe seu nome");
+        nome_funcionario = ler.next();
+        System.out.println(" ");
+        System.out.println("Informe o seu salario");
+        salario_funcionario = ler.nextFloat();
+        System.out.println(" ");
+        System.out.println("Informe seu CPF");
+        cpf_funcionario = ler.next();
+        System.out.println(" ");
+        System.out.println("Informe seu endereço");
+        endereco = ler.next();
+        
+        gerente maria = new gerente(nome_funcionario, cpf_funcionario, salario_funcionario, endereco);
+        
+        System.out.println("O Nome do Gerente é "+ maria.getNomeFuncionario());
+        System.out.println(" ");
+        System.out.println("O salario desse Gerente é "+ maria.getSalarioFuncionario());
+        System.out.println(" ");
+        System.out.println("O endereço desse Gerente é "+ maria.getEnderecoFuncionario());
 		
 	}
 
