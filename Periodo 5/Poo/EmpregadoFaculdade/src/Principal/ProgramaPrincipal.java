cc7bfacb21b5ba84328e8daefd653d9d637c6ca9
joao123marcos <joaomarcos.spires@gmail.com>
2021-03-09 23:27:44 -0300

    teste

--- Periodo 5/Poo/EmpregadoFaculdade/src/Principal/ProgramaPrincipal.java
+++ Periodo 5/Poo/EmpregadoFaculdade/src/Principal/ProgramaPrincipal.java
@@ -52,7 +52,7 @@ public class ProgramaPrincipal {
 		
 		System.out.println(" ");
 		
-		relatorio.relatorio(maria);
+		
 
 		//System.out.println("Seu nome "+maria.getNome());
 		//System.out.println("Seu salario "+maria.getGastos());
